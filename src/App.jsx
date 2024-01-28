37f60502c9f72a119306566522fc9b414bf16c46
naveenterance <naveenterance@gmail.com>
2024-01-28 10:02:51 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { ContextProvider } from "./context/Context";
 const App = () => {
   return (
     <ContextProvider>
-      <div className="lg:flex bg-slate-800 h-screen">
-        <div className="lg:w-1/3  h-full bg-slate-300 rounded-lg m-4 p-2">
+      <div className="lg:flex bg-slate-800  pb-16 pt-2 h-screen lg:h-full">
+        <div className="lg:w-1/3  h-full bg-slate-300 rounded-lg mx-4 p-2">
           <Form />
         </div>
         <div className="lg:w-2/3  mt-4">
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -10,7 +10,7 @@ const View = () => {
 
   return (
     <div>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 m-2">
+      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-2  p-4 bg-slate-800">
         {state.entries.map((data, index) => (
           <li
             key={index}
