8593bee429ce34c8f34a5f5033aceef302e78a2b
naveenterance <naveenterance@gmail.com>
2024-01-28 09:48:07 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { ContextProvider } from "./context/Context";
 const App = () => {
   return (
     <ContextProvider>
-      <div className="lg:flex bg-slate-800 ">
+      <div className="lg:flex bg-slate-800 h-screen">
         <div className="lg:w-1/3  h-full bg-slate-300 rounded-lg m-4 p-2">
           <Form />
         </div>
